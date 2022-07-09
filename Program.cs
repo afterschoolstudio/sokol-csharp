0fe4488d9c42d7f07e0639eda3c9529e11739f32
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2022-07-08 21:48:43 -0400

    more tries

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-Console.WriteLine("Hello, World!");
 // ConsoleDriver.Run(new SokolLib());
 // See https://aka.ms/new-console-template for more information
 // var x = new Sokol.sokol_app.
@@ -13,19 +12,23 @@ Console.WriteLine("Hello, World!");
 // while(true)
 // unsafe
 // {
-    using(var desc = new Sokol.SappDesc(){
-        InitCb = InitCallback,
+
+
+    Sokol.sokol_app.SappRun(new Sokol.SappDesc(){
+        InitCb = () => {
+            Console.WriteLine("init");
+            Sokol.sokol_gfx.SgSetup(new Sokol.SgDesc(){
+                Context = new Sokol.SgContextDesc(){}
+            });
+        },
         FrameCb = FrameCallback,
-        CleanupCb = CleanupCallback,
+        CleanupCb = CleanupCallback, 
         EventCb = EventCallback,
         Width = 1280,
         Height = 720,
         SampleCount = 4,
         WindowTitle = "SOKOL WINDOW",
-    })
-    {
-        Sokol.sokol_app.SappRun(desc);
-    }
+    });
     // var desc = new Sokol.SappDesc(){
     //     InitCb = InitCallback,
     //     FrameCb = FrameCallback,
@@ -53,28 +56,42 @@ Console.WriteLine("Hello, World!");
 
     static void CleanupCallback()
     {
+        Sokol.sokol_gfx.SgShutdown();
         Console.WriteLine("cleanup called");
     }
 
     // [UnmanagedCallersOnly]
-    static void InitCallback()
-    {
-        // unsafe
-        // {
-        //     Sokol.sokol_gfx.SgSetup(new Sokol.SgDesc(){
-        //         Context = new Sokol.SgContextDesc(){}
-        //     });
-        // }
-        Console.WriteLine("init called");
-        var gfxDesc = new Sokol.SgDesc(){
-            Context = new Sokol.SgContextDesc(){}
-        };
-        // GCHandle gch_gfx = GCHandle.Alloc(gfxDesc,GCHandleType.Pinned);
-        Sokol.sokol_gfx.SgSetup(gfxDesc);
-    }
+    // static void InitCallback() //doesn't work, needs to be in function itself?
+    // {
+    //     // unsafe
+    //     // {
+    //     //     Sokol.sokol_gfx.SgSetup(new Sokol.SgDesc(){
+    //     //         Context = new Sokol.SgContextDesc(){}
+    //     //     });
+    //     // }
+    //     Console.WriteLine("init called");
+    //     var gfxDesc = new Sokol.SgDesc(){
+    //         Context = new Sokol.SgContextDesc(){}
+    //     };
+    //     // GCHandle gch_gfx = GCHandle.Alloc(gfxDesc,GCHandleType.Pinned);
+        
+    // }
 
     static void FrameCallback()
     {
+        var passAction = new Sokol.SgPassAction();
+        passAction.Colors[0] = new Sokol.SgColorAttachmentAction(){
+            Action = Sokol.SgAction.SG_ACTION_CLEAR,
+            Value = new Sokol.SgColor(){
+                R = 1.0f,
+                G = 0f,
+                B = 0f,
+                A = 1.0f,
+            }
+        };
+        Sokol.sokol_gfx.SgBeginDefaultPass(passAction,Sokol.sokol_app.SappWidth(),Sokol.sokol_app.SappHeight());
+        Sokol.sokol_gfx.SgEndPass();
+        Sokol.sokol_gfx.SgCommit();
         Console.WriteLine("frame called");
     }
 
